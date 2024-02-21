6d66aea09c81cefa6f9ed5262a7aaebe863b5e2d
Ariel Abreu <facekapow@outlook.com>
2024-02-20 23:21:16 -0500

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Nix package for Pacrat";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "github:edolstra/flake-compat";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      utils,
+      ...
+    }:
+    utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pacratServerPkg = pkgs.buildGoModule {
+          pname = "pacrat-server";
+          version = "0.1.0";
+
+          src = ./.;
+
+          vendorHash = null;
+
+          modRoot = "./server";
+
+          preInstall =
+            ''
+              mv $GOPATH/bin/{server,pacrat-server}
+            '';
+
+          meta = with nixpkgs.lib; {
+            description = "A simple Arch Linux custom repository manager";
+            homepage = "https://git.facekapow.dev/facekapow/pacrat";
+            license = licenses.agpl3;
+            maintainers = [];
+          };
+        };
+      in
+      {
+        packages.default = pacratServerPkg;
+        legacyPackages.pacrat-server = pacratServerPkg;
+        nixosModules.default = { ... }: {
+          imports = [ ./module.nix ];
+        };
+      }
+    );
+}
---
+++ module.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.services.pacrat;
+  settingsFormat = pkgs.formats.toml;
+  settingsFile = settingsFormat.generate "pacrat.toml" cfg.settings;
+in
+{
+  options = {
+    services.pacrat = {
+      enable = mkEnableOption (mdDoc "A simple Arch Linux custom repository manager");
+
+      settings = mkOption {
+        type = types.submodule {
+          freeformType = settingsFormat.type;
+          options = {};
+        };
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.pacrat = {
+      description = "A simple Arch Linux custom repository manager";
+      wantedBy = "multi-user.target";
+      after = [ "network.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.pacrat}/bin/pacrat-server -c ${settingsFile}";
+        WorkingDirectory = "/var/lib/pacrat";
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        PrivateMounts = "yes";
+        PrivateTmp = "yes";
+        ProtectSystem = "strict";
+        ProtectHome = "yes";
+        ProtectControlGroups = true;
+        ProtectHostname = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ReadWritePaths = [
+          "/var/lib/pacrat"
+        ];
+        RemoveIPC = true;
+        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallFilter = "@system-service";
+        SystemCallArchitectures = "native";
+      };
+    };
+
+    users.users.pacrat = {
+      group = "pacrat";
+      isSystemUser = true;
+    };
+    users.groups.pacrat = {};
+  };
+}
